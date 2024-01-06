d1dfbfd018e0e98224f7d0d6a7daf7e296988e78
hemchandhar <hemchandhar@gmail.com>
2024-01-06 14:09:01 +0530

    cron job created

--- cron_job.py
+++ cron_job.py
@@ -3,11 +3,11 @@ from crontab import CronTab
 import subprocess
 
 # Creating new cron tab
-cron = CronTab(user='your_username')
+cron = CronTab(user='Hemchandhar')
 
 # Setting the Python path and script path for execution
-python_path = '/path/to/your/python'
-script_path = '/path/to/your/app.py'
+python_path = 'C:\Python39\python.exe'
+script_path = 'H:\Assessments and Personal Projects\Securin Assessment\Python Task\app.py'
 
 # Adding a new cron job to trigger the script
 job = cron.new(command=f'{python_path} {script_path} sync_cves')
